1139257c1ea579324707aee48b7a4a9881a608b0
Adedamola Omoniyi <damolaominiyi@gmail.com>
2024-12-06 19:34:17 +0000

    This was just an initial test at setting up stuff

--- Test.py
+++ Test.py
@@ -1,15 +1,1 @@
-from direct.showbase.ShowBase import ShowBase
-from gui import GUI
-def mint(mine):
-    print(mine,"minted")
-
-mint(mine = 22)
-class Main(ShowBase):
-    def __init__(self):
-        super().__init__()
-        self.UI = GUI(self)
-        self.UI.title_menu()
-
-
-base = Main()
-base.run()
+# I TESTED CLONING AND WORKING WITH GIT YESSS!!
