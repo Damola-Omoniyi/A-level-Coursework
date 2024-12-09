7267b91951ba46e0ddf3288eb08080a2ad8e6500
Adedamola Omoniyi <damolaominiyi@gmail.com>
2024-12-09 17:05:11 +0000

    THE CODE UPLOADED IS VERY SHITTY WE NEED A MIRACLE TO GET IT WORK AND LOOK NICE cleaned up player class just added proper controls

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from direct.showbase.ShowBase import ShowBase
 from gui import GUI
 from controls import Controller
 from panda3d.core import *
+from crypto import Crypto
+from knight import Knight
 
 loadPrcFile("config/Config.prc")
 
-'''loadPrcFileData("", """
+''' loadPrcFileData("", """
     fullscreen true
     win-size 1920 1080
     aspect-ratio auto
-""")'''
-
+""") '''
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -46,13 +47,17 @@ class Main(ShowBase):
         #self.update_music()
         self.taskMgr.add(self.update_music, "music")
 
-        self.gamepad_nums = {"gamepad1": 0, "gamepad2": 1, "keyboard": 5, "CPU": 5}
+        self.gamepad_nums = {"gamepad1": 0, "gamepad2": 1, "keyboard": 2, "CPU": False}
 
+        self.characters = {"Crypto": Crypto, "Knight": Knight}
+        self.player1 = None
+        self.player2 = None
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 
     def check_start_game(self, task):
+        # print(self.player_data)
         if self.game_started is True:
             self.start_game()
             self.taskMgr.remove("START") # Remove this task after game starts so as not to waste processing power
@@ -63,32 +68,44 @@ class Main(ShowBase):
         self.UI.model.hide()  # hide model
         self.load_scene(self.scene_id)
         self.UI.in_game_gui()
+        player1_character = self.characters[self.player_data[0][1]]
+        print(player1_character)
+        player2_character = self.characters[self.player_data[1][1]]
+
+        self.player1 = player1_character(self.player_data[0][0], self)
+        self.player2 = player2_character(self.player_data[1][0], self)
+
+        self.player1.enemy = self.player2
+        self.player2.enemy = self.player1
+        #self.player1.start()
+        #self.player2.start()
+
 
     def update_music(self, task):
-        #print(f"Total-length: {self.current_song.length()}  Current-time {self.current_song.getTime()}")
+            #print(f"Total-length: {self.current_song.length()}  Current-time {self.current_song.getTime()}")
 
-        for song in self.music:
-            if self.can_play_song:
-                print(song)
-                #self.can_play_song = False
-                self.current_song = self.loader.loadMusic(song)
-                # YOU ARE SO SILLY!!! RESET VALUES RESET VALUE WE HAVE BEEN HERE FOR OVER 2 HOURS FOR JUST ONE LINE
-                self.current_song.play()
+            for song in self.music:
+                if self.can_play_song:
+                    #print(song)
+                    #self.can_play_song = False
+                    self.current_song = self.loader.loadMusic(song)
+                    # YOU ARE SO SILLY!!! RESET VALUES RESET VALUE WE HAVE BEEN HERE FOR OVER 2 HOURS FOR JUST ONE LINE
+                    self.current_song.play()
 
-                self.current_song.length()
-                self.current_song.getTime()
-                #if self.current_song.length() > self.current_song.getTime():
-                #    self.can_play_song = False
-                #else:
-                #    self.can_play_song = True
+                    self.current_song.length()
+                    self.current_song.getTime()
+                    # if self.current_song.length() > self.current_song.getTime():
+                    #    self.can_play_song = False
+                    # else:
+                    #    self.can_play_song = True
 
-        return  task.cont
+            return  task.cont
+
     def load_scene(self, scene_id):
         if scene_id == 1:
             self.scene = self.loader.loadModel("models/moonsurface/moonsurface.egg")
             self.scene.reparentTo(self.render)
             self.scene.setPos(0, -750, -250)
-
             sun = self.loader.loadModel("models/jack.egg.pz")
             sun.reparentTo(self.render)
             sun.setPos(0, 4000, 500)
