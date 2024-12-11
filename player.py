3d90c66626b2d76baf5debe287300fcf2d41956a
Adedamola Omoniyi <damolaominiyi@gmail.com>
2024-12-11 20:18:05 +0000

    So just added the code for the FSM bits

--- player.py
+++ player.py
@@ -41,4 +41,84 @@ class Player():
             self.base.accept(f"{gamepad_name}-dpad_left-up", print, ["left-up"])
             self.base.accept(f"{gamepad_name}-dpad_right", print, ["right"])
             self.base.accept(f"{gamepad_name}-dpad_right-up", print, ["right-up"])
+    def enterIdle(self):
+        self.player.loop("Idle")
+
+    def exitIdle(self):
+        self.player.stop()
+
+    def enterWalk(self):
+        self.player.loop("Walk")
+        self.player.setPlayRate(1.5, "Walk")
+
+    def exitWalk(self):
+        self.player.stop()
+
+    def enterWalkback(self):
+        self.player.loop("Walk")
+        self.player.setPlayRate(-1.5, "Walk")
+
+    def exitWalkback(self):
+        self.player.stop()
+
+    def enterImpact(self):
+        self.player.play("Impact1")
+
+    def exitImpact(self):
+        self.player.stop()
+
+    def enterAttack1(self):
+        self.player.play("Attack1")
+        self.player.setPlayRate(1.75, "Attack1")
+
+    def exitAttack1(self):
+        self.player.stop()
+
+    def enterAttack2(self):
+        self.player.play("Attack2")
+        self.player.setPlayRate(1.75, "Attack2")
+
+    def exitAttack2(self):
+        self.player.stop()
+
+    def enterAttack3(self):
+        self.player.play("Attack3")
+        self.player.setPlayRate(1.75, "Attack3")
+
+    def exitAttack3(self):
+        self.player.stop()
+
+    def enterAttack4(self):
+        self.player.play("Attack4")
+        self.player.setPlayRate(1.75, "Attack4")
+
+    def exitAttack4(self):
+        self.player.stop()
+
+    def enterBlock(self):
+        self.player.loop("Block1")
+
+    def exitBlock(self):
+        self.player.stop()
+
+    def enterJump(self):
+        self.player.play("Jump")
+
+    def exitJump(self):
+        self.player.stop()
+
+    def enterDeath(self):
+        self.player.play("Death1")
+
+    def exitDeath(self):
+        self.player.stop()
+
+    def enterSpecial1(self):
+        self.player.play("Special1")
+    def enterSpecial2(self):
+        self.player.play("Special2")
+    def exitSpecial1(self):
+        self.player.stop()
+    def exitSpecial2(self):
+        self.player.stop()
 
