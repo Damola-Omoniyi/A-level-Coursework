d86c6479a7715b213a436311f0c85d7a9155efa2
Adedamola Omoniyi <131362017+Damola-Omoniyi@users.noreply.github.com>
2025-02-04 10:18:31 +0000

    Setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+setup(
+    name='maxim',
+    options={
+        'build_apps': {
+            # Build asteroids.exe as a GUI application
+            'gui_apps': {
+                'maxim': 'main.py',
+            },
+
+            # Set up output logging, important for GUI apps!
+            'log_filename': '$USER_APPDATA/Maxim/output.log',
+            'log_append': False,
+
+            # Specify which files are included with the distribution
+            'include_patterns': [
+                '**/*.png',
+                '**/*.jpg',
+                '**/*.egg',
+                '**/*.wav',
+                '**/*.bam',
+                '**/*.mp3',
+
+
+
+            ],
+
+            "gui_apps": {
+    "main": "src/main.py",
+},
+"icons": {
+    # The key needs to match the key used in gui_apps/console_apps.
+    # Alternatively, use "*" to set the icon for all apps.
+    "maxim": ["icon-16.png"],
+},
+
+            # Include the OpenGL renderer and OpenAL audio plug-in
+            'plugins': [
+                'pandagl',
+                'p3openal_audio',
+            ],
+        }
+    }
+)
+
+# python setup.py build_apps
